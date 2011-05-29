d49b9d04f7a7cfd1dabb548fc5ac5156cfd32f5d
Chris Homer <chris@thredup.com>
2011-05-28 17:49:57 -0700

    Initial starting point - clean skeleton rc1 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubboard::Application.config.secret_token = 'f4e47812f3833182be898ecf1531363979ba933ac9864196d6e13b49da3bce850c7bfb07e5cad4a8b6bd34facd35207fce180d571e8d0f1e79e73e9fed342d35'
